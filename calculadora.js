a4ca1b30b458e5cf656590cedc3549e9137f1598
aleesousa <aleepsousa010@gmail.com>
2025-03-12 23:39:54 -0300

    Exercícios 04

---
+++ calculadora.js
@@ -0,0 +1,18 @@
+export function soma(a, b) {
+  return a + b;
+}
+
+export function subtracao(a, b) {
+  return a - b;
+}
+
+export function multiplicacao(a, b) {
+  return a * b;
+}
+
+export function divisao(a, b) {
+  if (b === 0) {
+    throw new Error("Divisão por zero não é permitida");
+  }
+  return a / b;
+}
